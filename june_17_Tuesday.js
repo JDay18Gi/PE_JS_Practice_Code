c1c29fa1780034492afc1a3bb859a6387f68b039
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-06-17 23:38:25 -0400

    js object practice for the day

--- june_17_Tuesday.js
+++ june_17_Tuesday.js
@@ -246,3 +246,356 @@ function warningMaker(obstacle) {
 
 warningMaker("iceberg")(5, "Jupiter");
 
+// BEGIN OBJECT EXERCISES
+// BEGIN OBJECT EXERCISES
+// BEGIN OBJECT EXERCISES
+// BEGIN OBJECT EXERCISES
+// BEGIN OBJECT EXERCISES
+// BEGIN OBJECT EXERCISES
+
+// creat object literals 1
+// creat object literals 2
+// creat object literals 3
+
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"]
+};
+
+var Economist = {
+	url: "http://www.economist.com/",
+	searchLocation: ".mh-shirt",
+	articleContent: "div .main-content"
+};
+
+var Forbes = {
+	name: "Forbes",
+	url: "http://www.forbes.com/",
+	searchLocation: ".search",
+	articleContent: ".article-injected-body"
+};
+
+/// loop through and find item 1
+
+var objects = [BlogPost, Economist, Forbes];
+
+function returnURL(name, list) {
+	for(var i = 0; i < list.length; i++) {
+		console.log(i);
+		if (name === list[i].name) {
+			console.log(list[i].url);
+		}
+	}
+};
+
+returnURL("Forbes", objects);
+
+/// loop through and find item 2
+
+var objects = [BlogPost, Economist, Forbes];
+
+function returnURL(name, list) {
+	for(var i = 0; i < list.length; i++) {
+		console.log(i);
+		if (name === list[i].name) {
+			console.log(list[i]["url"]);
+		}
+	}
+};
+
+returnURL("Forbes", objects);
+
+
+/// loop through and find item 3
+
+var book = {
+	title: "title",
+	eval: "boring",
+	other: "message"
+}
+
+function searchBook(object) {
+	for (property in object) {
+		if (object.property = "boring") {
+			return "Throw away this object";
+		}
+	}
+}
+
+// remove properties add array and find item within array 1
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"]
+};
+
+var Forbes = {
+	name: "Forbes",
+	url: "http://www.forbes.com/",
+	searchLocation: ".search",
+	articleContent: ".article-injected-body"
+};
+
+delete Forbes.name;
+
+Forbes.name;
+
+Forbes.name = ["Forbes", "Magazine"];
+
+Forbes.name[1];
+Forbes["name"][1];
+
+
+// remove properties add array and find item within array 2
+
+var Economist = {
+	url: "http://www.economist.com/",
+	searchLocation: ".mh-shirt",
+	articleContent: "div .main-content"
+};
+
+delete Economist.url;
+
+Economist.url;
+
+Economist.url = ["www", "ddd", "ccc"]
+
+Economist.url[0]
+
+
+// remove properties add array and find item within array 3
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"]
+};
+
+delete BlogPost["title"];
+
+BlogPost.title;
+
+BlogPost["title"] = "title";
+
+BlogPost.title;
+
+
+// function to create an ojbect within a object 1
+
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"]
+};
+
+function addTags(post, category, tagsArray) {
+	post.tags = {
+		"category": category,
+		"tags": tagsArray
+	};
+}
+
+addTags(BlogPost, "Economics", ["velocity", "inflation", "oil"]);
+
+BlogPost.tags.category;
+
+BlogPost.tags.tags[1];
+
+
+
+
+// function to create an ojbect within a object 2
+
+var Economist = {
+	url: "http://www.economist.com/",
+	searchLocation: ".mh-shirt",
+	articleContent: "div .main-content"
+};
+
+function addKeyWords(publication, keytermsArray) {
+	publication.keyWords = {
+		searchLocation: publication.searchLocation,
+		keyterms: keytermsArray
+	};
+}
+
+addKeyWords(Economist, ["inflation", "money", "middle-east"]);
+
+Economist.keyWords.searchLocation;
+
+Economist.keyWords.keyterms[2];
+
+// function to create an ojbect within a object 3
+
+var Forbes = {
+	name: "Forbes",
+	url: "http://www.forbes.com/",
+	searchLocation: ".search",
+	articleContent: ".article-injected-body",
+	numHits: 0
+};
+
+
+
+function addHit (publication, word, location) {
+	publication.numHits++;
+	publication["hit"+publication.numHits] = {
+		word: word,
+		location: location
+	}
+}
+
+addHit(Forbes, "velocity", "bottom");
+
+addHit(Forbes, "velocity", "top");
+
+Forbes.hit2.location;
+
+
+// add function to object 1
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"],
+	addTags: function(category, tagsArray) {
+		this.tags = {
+			"category": category,
+			"tags": tagsArray
+		};
+	}
+};
+
+
+BlogPost.addTags("Economics", ["velocity", "inflation", "oil"]);
+
+BlogPost.tags.category;
+
+BlogPost.tags.tags[1];
+
+
+// add function to object 2
+
+var Economist = {
+	url: "http://www.economist.com/",
+	searchLocation: ".mh-shirt",
+	articleContent: "div .main-content",
+	addKeyWords: function(keytermsArray) {
+		this.keyWords = {
+			searchLocation: this.searchLocation,
+			keyterms: keytermsArray
+		};
+	}
+};
+
+
+
+Economist.addKeyWords(["inflation", "money", "middle-east"]);
+
+Economist.keyWords.searchLocation;
+
+Economist.keyWords.keyterms[2];
+
+
+// add function to object 3
+
+var Forbes = {
+	name: "Forbes",
+	url: "http://www.forbes.com/",
+	searchLocation: ".search",
+	articleContent: ".article-injected-body",
+	numHits: 0,
+	addHit: function( word, location) {
+		this.numHits++;
+		this["hit"+this.numHits] = {
+			word: word,
+			location: location
+		};
+	}
+};
+
+
+
+Forbes.addHit("velocity", "bottom");
+
+Forbes.addHit("velocity", "top");
+
+Forbes.hit2.location;
+
+
+//add new function to object 1
+
+var BlogPost = {
+	title: "The title",
+	content: "This is the contnet.",
+	images: ["image1.png", "image2.png", "image3.png"],
+	addTags: function(category, tagsArray) {
+		this.tags = {
+			"category": category,
+			"tags": tagsArray
+		};
+	}
+};
+
+BlogPost.changeContent = function(newContnet) {
+	this.content = newContnet;
+}
+
+BlogPost.changeContent("This is the new content!");
+
+BlogPost.content;
+
+
+//add new function to object 2
+
+
+var Economist = {
+	url: "http://www.economist.com/",
+	searchLocation: ".mh-shirt",
+	articleContent: "div .main-content",
+	addKeyWords: function(keytermsArray) {
+		this.keyWords = {
+			searchLocation: this.searchLocation,
+			keyterms: keytermsArray
+		};
+	}
+};
+
+Economist.deleteProperty = function(property) {
+	delete this[property];
+}
+
+Economist.deleteProperty("url");
+
+Economist.url;
+
+//add new function to object 3
+
+
+var Forbes = {
+	name: "Forbes",
+	url: "http://www.forbes.com/",
+	searchLocation: ".search",
+	articleContent: ".article-injected-body",
+	numHits: 0,
+	addHit: function( word, location) {
+		this.numHits++;
+		this["hit"+this.numHits] = {
+			word: word,
+			location: location
+		};
+	}
+};
+
+Forbes.addtoHits = function(num) {
+	this.numHits += num;
+}
+
+Forbes.addtoHits(5);
+
+Forbes.numHits;
+
